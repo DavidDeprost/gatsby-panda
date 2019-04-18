aef774176908dfd44ae18b03924a4fe8a6fa8b59
DavidDeprost <dadeprost@gmail.com>
2019-04-18 12:33:01 +0200

    Gatsby tutorial part 7

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
-import { graphql } from "gatsby"
 import { css } from "@emotion/core"
+import { Link, graphql } from "gatsby"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
 
@@ -34,23 +34,31 @@ export default ({ data }) => {
         <h4>{postCount} {postCount > 1 ? `Posts` : `Post`}</h4>
 
         {data.allMarkdownRemark.edges.map(({ node }) => (
-          <div key={node.id}>
-            <h3
-              css={css`
-                margin-bottom: ${rhythm(1 / 4)};
-              `}
-            >
-              {node.frontmatter.title}{" "}
-              <span
-                css={css`
-                  color: #bbb;
-                `}
-              >
-                — {node.frontmatter.date}
-              </span>
-            </h3>
-            <p>{node.excerpt}</p>
-          </div>
+            <div key={node.id}>
+                <Link
+                  to={node.fields.slug}
+                  css={css`
+                    text-decoration: none;
+                    color: inherit;
+                  `}
+                >
+                    <h3
+                      css={css`
+                        margin-bottom: ${rhythm(1 / 4)};
+                      `}
+                    >
+                      {node.frontmatter.title}{" "}
+                      <span
+                        css={css`
+                          color: #bbb;
+                        `}
+                      >
+                        — {node.frontmatter.date}
+                      </span>
+                    </h3>
+                    <p>{node.excerpt}</p>
+                </Link>
+            </div>
         ))}
       </div>
     </Layout>
@@ -67,6 +75,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
